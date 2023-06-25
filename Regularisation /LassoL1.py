760e0b3983b80b662c7562af51b79a2206aea8df
Kavita Rana <97081881+KaviRana@users.noreply.github.com>
2023-06-25 18:25:22 +0530

    Update LassoL1.py

--- Regularisation /LassoL1.py
+++ Regularisation /LassoL1.py
@@ -40,3 +40,51 @@ plt.legend()
 plt.title('Comparison of Polynomial Regression with and without L1 Regularization')
 plt.grid(True)
 plt.show()
+#//////////////////////////////////////////////////////////////////////
+#Hypertuning Parameters
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Lasso
+from sklearn.datasets import make_regression
+
+# Generate a synthetic regression dataset
+X, y = make_regression(n_samples=100, n_features=1, noise=20)
+
+# Create polynomial features
+poly = PolynomialFeatures(degree=10)
+X_poly = poly.fit_transform(X)
+
+# Define alpha values to compare
+alpha_values = [0.01, 0.1, 1.0, 10.0]
+
+# Initialize Lasso models for different alpha values
+lasso_models = []
+colors = ['#3AA6B9', 'green', 'red', 'purple']
+
+
+# Plot the original data points
+plt.scatter(X, y, color='#FF9EAA', label='Original Data')
+
+# Plot the fitted curves for different alpha values
+X_plot = np.linspace(np.min(X), np.max(X), 100).reshape(-1, 1)
+X_plot_poly = poly.transform(X_plot)
+
+for alpha, color in zip(alpha_values, colors):
+    lasso = Lasso(alpha=alpha)
+    lasso.fit(X_poly, y)
+    y_pred = lasso.predict(X_plot_poly)
+    plt.plot(X_plot, y_pred, color=color, linewidth=2, label='Lasso (alpha={})'.format(alpha))
+
+# Set plot labels and legend
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.title('Hyperparameter Tuning')
+
+
+# Show the plot
+plt.show()
+
+
+
